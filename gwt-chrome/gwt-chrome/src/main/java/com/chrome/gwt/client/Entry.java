05e815f2987a0f48e40d3089277d70b0f1a63aaa
bakker.mark <bakker.mark@2b2c9f42-f564-11de-a799-c55347f9a4a0>
2009-12-30 18:50:50 +0000

    refactor

---
+++ gwt-chrome/gwt-chrome/src/main/java/com/chrome/gwt/client/Entry.java
@@ -0,0 +1,12 @@
+package com.chrome.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Entry implements EntryPoint {
+
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
