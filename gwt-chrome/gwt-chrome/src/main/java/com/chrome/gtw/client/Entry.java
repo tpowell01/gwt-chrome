8d446d5fa2ad6c1aecfaa7b47d236f356438d68c
bakker.mark <bakker.mark@2b2c9f42-f564-11de-a799-c55347f9a4a0>
2009-12-30 18:33:36 +0000

    untested code

---
+++ gwt-chrome/gwt-chrome/src/main/java/com/chrome/gtw/client/Entry.java
@@ -0,0 +1,12 @@
+package com.chrome.gtw.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Entry implements EntryPoint {
+
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
